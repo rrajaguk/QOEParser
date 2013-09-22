8f2b34977fae9792b8c31d4a4e24ff148709d889
Ronald Rajagukguk <ronald.penalosa@gmail.com>
2013-09-22 22:31:01 +0800

    added bare GUI

--- QOEGui/Form1.Designer.cs
+++ QOEGui/Form1.Designer.cs
@@ -28,58 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.ValueTextBox = new System.Windows.Forms.RichTextBox();
+            this.TranslateButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // ValueTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(351, 195);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.ValueTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ValueTextBox.Name = "ValueTextBox";
+            this.ValueTextBox.Size = new System.Drawing.Size(351, 195);
+            this.ValueTextBox.TabIndex = 0;
+            this.ValueTextBox.Text = "";
             // 
-            // button1
+            // TranslateButton
             // 
-            this.button1.Location = new System.Drawing.Point(288, 213);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Translate!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.TranslateButton.Location = new System.Drawing.Point(288, 213);
+            this.TranslateButton.Name = "TranslateButton";
+            this.TranslateButton.Size = new System.Drawing.Size(75, 23);
+            this.TranslateButton.TabIndex = 1;
+            this.TranslateButton.Text = "Translate!";
+            this.TranslateButton.UseVisualStyleBackColor = true;
+            this.TranslateButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(397, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(529, 388);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(397, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(529, 388);
+            this.dataGridView.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(938, 412);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.TranslateButton);
+            this.Controls.Add(this.ValueTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox ValueTextBox;
+        private System.Windows.Forms.Button TranslateButton;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
--- QOEGui/Form1.cs
+++ QOEGui/Form1.cs
@@ -6,18 +6,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using QOEParser;
 
 namespace QOEGui
 {
     public partial class Form1 : Form
     {
+        Composer composer;
         public Form1()
         {
             InitializeComponent();
+            composer = new Composer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            XElement definition = XElement.Load(Application.StartupPath + @"\QOE_Sample.xml");
+            composer.ParseValueDefinition(definition);
+            PairResult[] res = composer.getValue(ValueTextBox.Text);
+            dataGridView.DataSource = res;
 
         }
     }
